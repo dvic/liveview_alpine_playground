34c891a9a826ae2940c16a4fd2d6b96f3e885d7a
dvic <info@dvic.io>
2020-09-09 15:01:15 +0200

    add example clicked twice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
+import "alpinejs";
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss"
+import "../css/app.scss";
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -12,22 +13,37 @@ import "../css/app.scss"
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html"
-import {Socket} from "phoenix"
-import NProgress from "nprogress"
-import {LiveSocket} from "phoenix_live_view"
+import "phoenix_html";
+import { Socket } from "phoenix";
+import NProgress from "nprogress";
+import { LiveSocket } from "phoenix_live_view";
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+window.item_clicked = (event) => {
+  console.log(event.target);
+};
+
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+  dom: {
+    onBeforeElUpdated(from, to) {
+      if (from.__x) {
+        window.Alpine.clone(from.__x, to);
+      }
+    },
+  },
+});
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", info => NProgress.start())
-window.addEventListener("phx:page-loading-stop", info => NProgress.done())
+window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
+window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
 
 // connect if there are any LiveViews on the page
-liveSocket.connect()
+liveSocket.connect();
 
 // expose liveSocket on window for web console debug logs and latency simulation:
 // >> liveSocket.enableDebug()
 // >> liveSocket.enableLatencySim(1000)
-window.liveSocket = liveSocket
+window.liveSocket = liveSocket;
--- lib/demo_web/live/page_live.ex
+++ lib/demo_web/live/page_live.ex
@@ -3,37 +3,39 @@ defmodule DemoWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
+    if connected?(socket) do
+      Process.send_after(self(), :add, 1000)
+    end
+
+    {:ok, assign(socket, items: [])}
   end
 
   @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
+  def handle_info(:add, socket) do
+    {:noreply, socket |> assign(items: [DateTime.utc_now() | socket.assigns.items])}
   end
 
   @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not DemoWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
-    end
-
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+  def render(assigns) do
+    ~L"""
+      <div x-data>
+        <ul>
+          <li id="fixed_item" @click="item_clicked($event)">
+            <div><span>the fixed item</span></div>
+          </li>
+          <%= for {_, idx} <- @items |> Enum.with_index() do %>
+          <li id="item_<%= idx %>" @click="item_clicked($event)">
+            <div><span>alpinejs on click li (this one calls the handler twice)</span></div>
+          </li>
+          <li x-data id="item_<%= idx %>_scope" @click="item_clicked($event)">
+            <div><span>alpinejs on click li with x-data on li</span></div>
+          </li>
+          <li id="item_<%= idx %>_native" onClick="item_clicked(event)">
+            <div><span>native on click li</span></div>
+          </li>
+          <% end %>
+        </ul>
+      </div>
+    """
   end
 end
